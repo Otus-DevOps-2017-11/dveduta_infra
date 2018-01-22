8a24ca90534c6773d948e2f944ea01fbb26d8de9
Denis Veduta <dveduta@ivi.ru>
2018-01-23 01:26:08 +0300

    tf before vars

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+provider "google" {
+    version = "1.4.0"
+    project = "beaming-pillar-188917"
+    region = "europe-west1"
+}
+
+resource "google_compute_instance" "app" {
+    name = "reddit-app"
+    machine_type = "g1-small"
+    zone = "europe-west1-b"
+    tags = ["reddit-app"]
+    boot_disk {
+        initialize_params {
+            image = "reddit-base"
+        }
+    }
+    network_interface {
+        network = "default"
+        access_config {}
+    }
+
+    metadata {
+        sshKeys = "dveduta:${file("~/.ssh/dveduta.pub")}"
+    }
+
+    connection {
+        type = "ssh"
+        user = "dveduta"
+        agent = false
+        private_key = "${file("~/.ssh/dveduta")}"
+    }
+
+    provisioner "file" {
+        source = "files/puma.service"
+        destination = "/tmp/puma.service"
+    }
+
+    provisioner "remote-exec" {
+        script = "files/deploy.sh"
+    }
+
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+    name = "allow-puma-default"
+    network = "default"
+    allow {
+        protocol = "tcp"
+        ports = ["9292"]
+    }
+    source_ranges = ["0.0.0.0/0"]
+    target_tags = ["reddit-app"]
+}
