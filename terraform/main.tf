bb4091c5940adfe3fb88c3e78ab4b0cd4bccc9a7
Denis Veduta <dveduta@ivi.ru>
2018-01-23 23:17:07 +0300

    fmt indents; more variables; example tfvars file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,53 +1,56 @@
 provider "google" {
-    version = "1.4.0"
-    project = "${var.project}"
-    region = "${var.region}"
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
 }
 
 resource "google_compute_instance" "app" {
-    name = "reddit-app"
-    machine_type = "g1-small"
-    zone = "europe-west1-b"
-    tags = ["reddit-app"]
-    boot_disk {
-        initialize_params {
-            image = "${var.disk_image}"
-        }
-    }
-    network_interface {
-        network = "default"
-        access_config {}
-    }
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "${var.instance_zone}"
+  tags         = ["reddit-app"]
 
-    metadata {
-        sshKeys = "dveduta:${file(var.public_key_path)}"
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
     }
+  }
 
-    connection {
-        type = "ssh"
-        user = "dveduta"
-        agent = false
-        private_key = "${file("~/.ssh/dveduta")}"
-    }
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
 
-    provisioner "file" {
-        source = "files/puma.service"
-        destination = "/tmp/puma.service"
-    }
+  metadata {
+    sshKeys = "dveduta:${file(var.public_key_path)}"
+  }
 
-    provisioner "remote-exec" {
-        script = "files/deploy.sh"
-    }
+  connection {
+    type        = "ssh"
+    user        = "dveduta"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
 
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-    name = "allow-puma-default"
-    network = "default"
-    allow {
-        protocol = "tcp"
-        ports = ["9292"]
-    }
-    source_ranges = ["0.0.0.0/0"]
-    target_tags = ["reddit-app"]
+  name    = "allow-puma-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
 }
