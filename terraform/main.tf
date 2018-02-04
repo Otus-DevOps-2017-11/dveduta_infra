b4c0eeee3d62898cbf388d0f9b759669730d6ceb
Denis Veduta <dveduta@ivi.ru>
2018-02-04 22:51:56 +0300

    backup before module reuse

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,72 +4,21 @@ provider "google" {
   region  = "${var.region}"
 }
 
-resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = "${var.instance_zone}"
-  tags         = ["reddit-app"]
-
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config = {
-      nat_ip = "${google_compute_address.app_ip.address}"
-    }
-  }
-
-  metadata {
-    sshKeys = "dveduta:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "dveduta"
-    agent       = false
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
+module "app" {
+  source          = "modules/app"
+  public_key_path = "${var.public_key_path}"
+  instance_zone   = "${var.instance_zone}"
+  app_disk_image  = "${var.app_disk_image}"
 }
 
-resource "google_compute_address" "app_ip" {
-  name = "reddit-app-ip"
+module "db" {
+  source          = "modules/db"
+  public_key_path = "${var.public_key_path}"
+  instance_zone   = "${var.instance_zone}"
+  db_disk_image   = "${var.db_disk_image}"
 }
 
-resource "google_compute_firewall" "firewall_puma" {
-  name    = "allow-puma-default"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app"]
-}
-
-resource "google_compute_firewall" "firewall_ssh" {
-  name    = "default-allow-ssh"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
+module "vpc" {
+  source        = "modules/vpc"
+  source_ranges = ["0.0.0.0"]
 }
